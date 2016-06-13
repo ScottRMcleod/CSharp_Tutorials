5853da827cf86443faf512807ee9c1456553e9c4
Scott Mcleod <smcleod_06@hotmail.com>
2016-06-13 21:37:40 +1000

    Add files via upload

---
+++ MVC-WebApp1/Controller/CheckingAccountController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CheckingAccountController : Controller
+    {
+        // GET: CheckingAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CheckingAccount/Details/5
+        public ActionResult Details()
+        {
+            var checkingAccount = new CheckingAccount { AccountNumber = "0000123456", FirstName = "Micheal", LastName = "Sullivan", Balance = 500 };
+            return View(checkingAccount);
+        }
+
+        // GET: CheckingAccount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var checkingAccount = new CheckingAccount { AccountNumber = "0000123456", FirstName = "Scott", LastName = "Mcleod", Balance = 1000000 };
+            return View(checkingAccount);
+        }
+
+        // POST: CheckingAccount/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC-WebApp1/Controller/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    //Get / home/index
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Having trouble send us a message.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.TheMessage = "Thank you, One of our consultants will be in contact with you shortley";
+
+            return View();
+        }
+        public ActionResult Foo()
+        {
+     
+            return View("About");
+        }
+
+        public ActionResult Serail(string letterCase)
+        {
+            //this applicatins
+            string serail = "ASPNETMVC5ATM1";
+
+            //Created an If statement passing
+            if (letterCase == "lower")
+            {
+                return Content(serail.ToLower());
+            }
+            //else
+            //  return Content(serail.ToUpper());
+
+            //return Json(new { name = "serial", value = serail}, JsonRequestBehavior.AllowGet);
+            //return Content(serail);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
